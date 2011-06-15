409008984f4d55ddcb9eaa209b3321d308693a61
danielwiener <daniel@danielwiener.com>
2011-06-14 20:15:23 -0400

    a test

--- footer.php
+++ footer.php
@@ -3,11 +3,11 @@
         <div class="footer clear">
             <div class="footer_inn">
                 <div class="left">
-                    <p>&copy; 2010 <a href="<?php bloginfo('home'); ?>"><?php bloginfo('name'); ?></a></p>
+                    <p>&copy; 2000 - <?php echo date('Y'); ?> <a href="<?php bloginfo('home'); ?>"><?php bloginfo('name'); ?></a> | <a href="/lc/contact">Contact</a></p>
                     <!-- <p>Powered by <a href="http://wordpress.org">Wordpress</a></p> -->
                 </div>
                 <div class="right">
-                    <p>&nbsp;<!-- Designed by <a href="http://wpshower.com" class="wpshower_logo">Wpshower</a> --></p>
+                    <p>&nbsp;<!-- Designed by <a href="http://wpshower.com" class="wpshower_logo">Wpshower</a> adding a test--></p>
                 </div>
             </div>
         </div>
